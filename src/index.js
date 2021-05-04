c4af1e1a8b3270aeec45d119f0236c238348525b
Pratik <pratiksawant13@gmail.com>
2021-05-04 15:55:35 +1000

    feat: implement reducers, actions and store patterns

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+import { bugAdded, bugRemoved, bugResolved } from './actions';
+import store from './store';
+
+const unsubscribe = store.subscribe(() => {
+  console.log('Store changed!', store.getState());
+});
+
+store.dispatch(bugAdded('Bug 1'));
+store.dispatch(bugResolved(1));
+
+// unsubscribe();
+
+store.dispatch(bugRemoved(1));
+
+console.log(store.getState());
